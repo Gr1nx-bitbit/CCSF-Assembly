6756b58980e890a51e81cc1a12b9821ca838e1b6
Gr1nx <gr1nx614@gmail.com>
2024-07-21 14:59:04 -0700

    forgot some requirements

--- 6/lab-assignments/assignment-8.asm
+++ 6/lab-assignments/assignment-8.asm
@@ -9,26 +9,24 @@
         li $a2, 19
         li $a3, 41
 
+        jal f # call f
+
+        li $v0, 10 # the return value is stored in $v0 which gets overwritten here to end the program 
+        syscall
+
     f:
         blt $a0, $a1, returnLess # if $a0 < $a1, then branch to returnLess 
         beq $a0, $a1, returnEqual # if $a0 and $a1 are equal, branch to retrunEqual
         bgt $a0, $a1, returnGreater # if $a0 > $a1, then branch to returnGreater
 
     returnLess:
-        move $v1, $a2 # load third parameter into return register 
-        j end
+        move $v0, $a2 # load third parameter into return register 
+        jr $ra # jump back to instruction after jal to end the program
 
     returnEqual:
-        move $v1, $a3 # load fourth parameter into return register
-        j end
+        move $v0, $a3 # load fourth parameter into return register
+        jr $ra
 
     returnGreater:
-        add $v1, $a2, $a3 # store $a2 + $a3 in return register
-
-    end:
-        li $v0, 1
-        move $a0, $v1
-        syscall
-
-        li $v0, 10
-        syscall
+        add $v0, $a2, $a3 # store $a2 + $a3 in return register
+        jr $ra
